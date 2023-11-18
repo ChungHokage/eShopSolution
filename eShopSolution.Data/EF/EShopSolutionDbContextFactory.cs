841c2ac071f0b15aac6fd48649929a18c49878a4
ChungHokage <chunglight01@gmail.com>
2023-11-18 16:13:18 +0700

    design database by code first

---
+++ eShopSolution.Data/EF/EShopSolutionDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopSolutionDbContextFactory : IDesignTimeDbContextFactory<EShopDBContext>
+    {
+        public EShopDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionBuilder=new DbContextOptionsBuilder<EShopDBContext>();
+
+            optionBuilder.UseSqlServer(connectionString);
+            return new EShopDBContext(optionBuilder.Options);
+        }
+    }
+}
