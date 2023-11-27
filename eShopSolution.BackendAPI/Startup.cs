40328c51ed75263c4dfb24f7c4bf9eddf68db7f3
ChungHokage <chunglight01@gmail.com>
2023-11-27 23:13:51 +0700

    fix commit

--- eShopSolution.BackendAPI/Startup.cs
+++ eShopSolution.BackendAPI/Startup.cs
@@ -47,8 +47,7 @@ namespace eShopSolution.BackendAPI
             services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
             services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
             services.AddTransient<IUserService, UserService>();
-            services.AddControllers()
-                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<LoginRequestValidator>());
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop", Version = "v1" });
